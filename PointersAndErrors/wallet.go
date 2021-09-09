c97bd2230c5f2bc8abe9a45417732f1d6bc8692a
Eric Averitt <15947822+EricAveritt@users.noreply.github.com>
2021-09-09 09:37:40 -0400

    Pointers and Errors
    Added an error check by using "go get -u github.com/kisielk/errcheck" that will check for other cases not included in current tests

---
+++ PointersAndErrors/wallet.go
@@ -0,0 +1,41 @@
+package pointer
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Stringer interface {
+	String() string
+}
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	// fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
